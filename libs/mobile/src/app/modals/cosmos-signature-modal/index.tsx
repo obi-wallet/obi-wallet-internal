50605f1077c2433900b64bad610faf7c54832cf3
Jonas Keinholz <git@inyono.dev>
2023-01-13 19:22:45 +0100

    signature-modal: unify PhoneNumberBottomSheetContent

--- libs/mobile/src/app/modals/cosmos-signature-modal/index.tsx
+++ libs/mobile/src/app/modals/cosmos-signature-modal/index.tsx
@@ -28,7 +28,6 @@ import {
   makeMultisignedTx,
 } from "@cosmjs/stargate";
 import { createVestingAminoConverters } from "@cosmjs/stargate/build/modules";
-import { BottomSheetTextInput } from "@gorhom/bottom-sheet/src";
 import {
   createStargateClient,
   isAnyMultisigWallet,
@@ -39,8 +38,6 @@ import {
   MultisigWallet,
   RequestObiSignAndBroadcastPayload,
   SinglesigWallet,
-  TerraMultisig,
-  TerraMultisigWallet,
   Text,
   WalletType,
 } from "@obi-wallet/common";
@@ -53,7 +50,6 @@ import LinearGradient from "react-native-linear-gradient";
 import invariant from "tiny-invariant";
 
 import { createBiometricSignature } from "../../biometrics";
-import { InlineButton } from "../../button";
 import { createSigningCosmWasmClient } from "../../clients";
 import { lendFees } from "../../fee-lender-worker";
 import { Loader } from "../../loader";
@@ -62,19 +58,13 @@ import {
   BottomSheetRef,
 } from "../../screens/components/bottom-sheet";
 import { CheckIcon, Key, KeysList } from "../../screens/components/keys-list";
-import {
-  SecurityQuestionInput,
-  useSecurityQuestionInput,
-} from "../../screens/components/phone-number/security-question-input";
-import { SendMagicSmsButton } from "../../screens/components/phone-number/send-magic-sms-button";
-import { VerifyAndProceedButton } from "../../screens/components/phone-number/verify-and-proceed-button";
 import { useStore } from "../../stores";
-import { TextInput } from "../../text-input";
 import {
   parseSignatureTextMessageResponse,
   sendSignatureTextMessage,
 } from "../../text-message";
 import { ConfirmMessages } from "../signature-modal/confirm-messages";
+import { PhoneNumberBottomSheetContent } from "../signature-modal/phone-number-bottom-sheet-content";
 import { wrapMessages } from "./wrap-messages";
 
 export interface SignatureModalProps extends ModalProps {
@@ -332,19 +322,41 @@ export const SignatureModalMultisig = observer<
         multisig?.phoneNumber ? (
           <BottomSheet bottomSheetRef={phoneNumberBottomSheetRef}>
             <PhoneNumberBottomSheetContent
-              payload={multisig.phoneNumber}
-              wallet={wallet}
-              getMessage={getMessage}
-              onSuccess={(signature) => {
-                setSignatures((signatures) => {
-                  const { phoneNumber } = multisig;
-                  invariant(phoneNumber, "Expected phone number key to exist.");
-                  return new Map(
-                    signatures.set(phoneNumber.address, signature)
-                  );
-                });
+              securityQuestion={multisig.phoneNumber.securityQuestion}
+              onRequest={async (securityAnswer) => {
+                invariant(
+                  multisig.phoneNumber,
+                  "Expected phoneNumber key to exist"
+                );
 
-                phoneNumberBottomSheetRef.current?.close();
+                const message = await getMessage();
+                await sendSignatureTextMessage({
+                  phoneNumber: multisig.phoneNumber.phoneNumber,
+                  securityAnswer,
+                  message,
+                  demoMode: isMultisigDemoWallet(wallet),
+                  chainId: currentCosmosChainInformation.chainId,
+                });
+              }}
+              onConfirm={async (key) => {
+                const signature = await parseSignatureTextMessageResponse({
+                  key,
+                  demoMode: isMultisigDemoWallet(wallet),
+                });
+                if (signature) {
+                  setSignatures((signatures) => {
+                    const { phoneNumber } = multisig;
+                    invariant(
+                      phoneNumber,
+                      "Expected phone number key to exist."
+                    );
+                    return new Map(
+                      signatures.set(phoneNumber.address, signature)
+                    );
+                  });
+
+                  phoneNumberBottomSheetRef.current?.close();
+                }
               }}
             />
           </BottomSheet>
@@ -657,210 +669,3 @@ export function useSignatureModalProps({
     ];
   }
 }
-
-interface PhoneNumberBottomSheetContentProps {
-  payload: TerraMultisig["phoneNumber"] | Multisig["phoneNumber"];
-  wallet: TerraMultisigWallet | MultisigWallet | SinglesigWallet;
-
-  getMessage(): Promise<Uint8Array>;
-
-  onSuccess(signature: Uint8Array): void;
-}
-
-const PhoneNumberBottomSheetContent =
-  observer<PhoneNumberBottomSheetContentProps>(
-    ({ payload, wallet, getMessage, onSuccess }) => {
-      const { chainStore } = useStore();
-      const chainId = chainStore.currentChain;
-      const intl = useIntl();
-      const { securityAnswer, setSecurityAnswer } = useSecurityQuestionInput();
-
-      const [sentMessage, setSentMessage] = useState(false);
-      const [key, setKey] = useState("");
-
-      const [magicButtonDisabled, setMagicButtonDisabled] = useState(true); // Magic Button disabled by default
-      const [
-        magicButtonDisabledDoubleclick,
-        setMagicButtonDisabledDoubleclick,
-      ] = useState(false); // Magic Button disabled on button-click to prevent double-click
-
-      const [verifyButtonDisabled, setVerifyButtonDisabled] = useState(true); // Magic Button disabled by default
-      const [
-        verifyButtonDisabledDoubleclick,
-        setVerifyButtonDisabledDoubleclick,
-      ] = useState(false); // Magic Button disable on button-click
-
-      const minInputCharsSecurityAnswer = 3;
-      const minInputCharsSMSCode = 8;
-
-      useEffect(() => {
-        if (securityAnswer.length >= minInputCharsSecurityAnswer) {
-          setMagicButtonDisabled(false); // Enable Magic Button if checks are okay
-        } else {
-          setMagicButtonDisabled(true);
-        }
-      }, [magicButtonDisabled, setMagicButtonDisabled, securityAnswer]);
-
-      useEffect(() => {
-        if (key.length >= minInputCharsSMSCode) {
-          setVerifyButtonDisabled(false); // Enable Magic Button if checks are okay
-        } else {
-          setVerifyButtonDisabled(true);
-          setVerifyButtonDisabledDoubleclick(false);
-        }
-      }, [verifyButtonDisabled, setVerifyButtonDisabled, key]);
-
-      if (!payload) return null;
-
-      if (sentMessage) {
-        return (
-          <View
-            style={{
-              flexGrow: 1,
-              flex: 1,
-              paddingHorizontal: 20,
-              justifyContent: "space-between",
-            }}
-          >
-            <View>
-              <Text
-                style={{
-                  color: "#999CB6",
-                  fontSize: 14,
-                  marginTop: 10,
-                }}
-              >
-                <FormattedMessage
-                  id="signature.pasteresponse"
-                  defaultMessage="Paste in the response you received."
-                />
-              </Text>
-              <TextInput
-                placeholder={intl.formatMessage({
-                  id: "signature.smscodelabel",
-                  defaultMessage: "8-Digits SMS-Code",
-                })}
-                textContentType="oneTimeCode"
-                keyboardType="number-pad"
-                style={{ marginTop: 25 }}
-                value={key}
-                onChangeText={setKey}
-                CustomTextInput={BottomSheetTextInput}
-              />
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginTop: 24,
-                }}
-              >
-                <Text
-                  style={{ color: "rgba(246, 245, 255, 0.6)", fontSize: 12 }}
-                >
-                  <FormattedMessage
-                    id="signature.noresponselabel"
-                    defaultMessage="Didn't receive a response?"
-                  />
-                </Text>
-
-                <InlineButton
-                  label={intl.formatMessage({
-                    id: "signature.sendagain",
-                    defaultMessage: "Resend",
-                  })}
-                  onPress={async () => {
-                    const message = await getMessage();
-                    await sendSignatureTextMessage({
-                      phoneNumber: payload.phoneNumber,
-                      securityAnswer,
-                      message,
-                      demoMode: isMultisigDemoWallet(wallet),
-                      chainId,
-                    });
-                  }}
-                />
-              </View>
-            </View>
-
-            <VerifyAndProceedButton
-              onPress={async () => {
-                try {
-                  setVerifyButtonDisabledDoubleclick(true);
-                  const response = await parseSignatureTextMessageResponse({
-                    key,
-                    demoMode: isMultisigDemoWallet(wallet),
-                  });
-                  if (response) onSuccess(response);
-                  setVerifyButtonDisabledDoubleclick(false);
-                } catch (e) {
-                  const error = e as Error;
-                  setVerifyButtonDisabledDoubleclick(false);
-                  console.error(error);
-                  Alert.alert(
-                    intl.formatMessage({
-                      id: "general.error",
-                      defaultMessage: "Error",
-                    }) + "VerifyAndProceedButton (1)",
-                    error.message
-                  );
-                }
-              }}
-              disabled={
-                verifyButtonDisabledDoubleclick
-                  ? verifyButtonDisabledDoubleclick
-                  : verifyButtonDisabled
-              }
-            />
-          </View>
-        );
-      }
-
-      return (
-        <View
-          style={{
-            flexGrow: 1,
-            flex: 1,
-            paddingHorizontal: 20,
-            justifyContent: "space-between",
-          }}
-        >
-          <SecurityQuestionInput
-            disabled
-            securityQuestion={payload.securityQuestion}
-            securityAnswer={securityAnswer}
-            onSecurityAnswerChange={setSecurityAnswer}
-            CustomTextInput={BottomSheetTextInput}
-          />
-
-          <SendMagicSmsButton
-            onPress={async () => {
-              setMagicButtonDisabledDoubleclick(true);
-
-              try {
-                const message = await getMessage();
-                await sendSignatureTextMessage({
-                  phoneNumber: payload.phoneNumber,
-                  securityAnswer,
-                  message,
-                  demoMode: isMultisigDemoWallet(wallet),
-                  chainId,
-                });
-                setSentMessage(true);
-                setMagicButtonDisabledDoubleclick(false);
-              } catch (e) {
-                const error = e as Error;
-                setMagicButtonDisabledDoubleclick(false);
-                console.error(error);
-                Alert.alert("Sending SMS failed.", error.message);
-              }
-            }}
-            disabled={
-              magicButtonDisabledDoubleclick
-                ? magicButtonDisabledDoubleclick
-                : magicButtonDisabled
-            }
-          />
-        </View>
-      );
-    }
-  );
--- libs/mobile/src/app/modals/terra-signature-modal/index.tsx
+++ libs/mobile/src/app/modals/terra-signature-modal/index.tsx
@@ -1,4 +1,3 @@
-import { BottomSheetTextInput } from "@gorhom/bottom-sheet/src";
 import {
   createLcdClient,
   isAnyMultisigWallet,
@@ -26,7 +25,6 @@ import { Alert, ModalProps, View } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import invariant from "tiny-invariant";
 
-import { InlineButton } from "../../button";
 import { lendFees } from "../../fee-lender-worker";
 import { Loader } from "../../loader";
 import {
@@ -34,15 +32,9 @@ import {
   BottomSheetRef,
 } from "../../screens/components/bottom-sheet";
 import { CheckIcon, Key, KeysList } from "../../screens/components/keys-list";
-import {
-  SecurityQuestionInput,
-  useSecurityQuestionInput,
-} from "../../screens/components/phone-number/security-question-input";
-import { SendMagicSmsButton } from "../../screens/components/phone-number/send-magic-sms-button";
-import { VerifyAndProceedButton } from "../../screens/components/phone-number/verify-and-proceed-button";
 import { useStore } from "../../stores";
-import { TextInput } from "../../text-input";
 import { ConfirmMessages } from "../signature-modal/confirm-messages";
+import { PhoneNumberBottomSheetContent } from "../signature-modal/phone-number-bottom-sheet-content";
 import { useGasPrices } from "./hooks";
 import {
   BiometricsKey,
@@ -309,21 +301,43 @@ export const TerraSignatureModal = observer<TerraSignatureModalProps>(
           multisig?.phoneNumber ? (
             <BottomSheet bottomSheetRef={phoneNumberBottomSheetRef}>
               <PhoneNumberBottomSheetContent
-                payload={multisig.phoneNumber}
-                wallet={wallet}
-                multisig={multisig}
-                getTransactionInformation={getTransactionInformation}
-                onSuccess={(signature) => {
-                  setSignatures((signatures) => {
-                    const { phoneNumber } = multisig;
-                    invariant(
-                      phoneNumber,
-                      "Expected phone number key to exist."
-                    );
-                    return new Map(signatures.set("phoneNumber", signature));
-                  });
+                securityQuestion={multisig.phoneNumber.securityQuestion}
+                onRequest={async (securityAnswer) => {
+                  invariant(
+                    multisig.phoneNumber,
+                    "Expected phoneNumber key to exist"
+                  );
 
-                  phoneNumberBottomSheetRef.current?.close();
+                  const { signDoc } = await getTransactionInformation();
+                  const phoneNumberRequestKey = new PhoneNumberRequestKey({
+                    phoneNumber: multisig.phoneNumber.phoneNumber,
+                    securityAnswer,
+                    chainId: currentTerraChainInformation.chainId,
+                    wallet,
+                    multisig,
+                  });
+                  await phoneNumberRequestKey.createSignatureAmino(signDoc);
+                }}
+                onConfirm={async (key) => {
+                  const { signDoc } = await getTransactionInformation();
+                  const phoneNumberRequestKey = new PhoneNumberConfirmKey({
+                    key,
+                    wallet,
+                    multisig,
+                  });
+                  const signature =
+                    await phoneNumberRequestKey.createSignatureAmino(signDoc);
+                  if (signature) {
+                    setSignatures((signatures) => {
+                      const { phoneNumber } = multisig;
+                      invariant(
+                        phoneNumber,
+                        "Expected phone number key to exist."
+                      );
+                      return new Map(signatures.set("phoneNumber", signature));
+                    });
+                    phoneNumberBottomSheetRef.current?.close();
+                  }
                 }}
               />
             </BottomSheet>
@@ -530,220 +544,3 @@ export function useTerraSignatureModalProps({
     });
   }
 }
-
-interface PhoneNumberBottomSheetContentProps {
-  payload: TerraMultisig["phoneNumber"];
-  wallet: TerraMultisigWallet;
-  multisig: TerraMultisig;
-
-  getTransactionInformation(): Promise<{
-    transaction: Tx;
-    signDoc: SignDoc;
-  }>;
-
-  onSuccess(signature: SignatureV2): void;
-}
-
-const PhoneNumberBottomSheetContent =
-  observer<PhoneNumberBottomSheetContentProps>(
-    ({ payload, wallet, multisig, getTransactionInformation, onSuccess }) => {
-      const { chainStore } = useStore();
-      const chainId = chainStore.currentTerraChain;
-      const intl = useIntl();
-      const { securityAnswer, setSecurityAnswer } = useSecurityQuestionInput();
-
-      const [sentMessage, setSentMessage] = useState(false);
-      const [key, setKey] = useState("");
-
-      const [magicButtonDisabled, setMagicButtonDisabled] = useState(true); // Magic Button disabled by default
-      const [
-        magicButtonDisabledDoubleclick,
-        setMagicButtonDisabledDoubleclick,
-      ] = useState(false); // Magic Button disabled on button-click to prevent double-click
-
-      const [verifyButtonDisabled, setVerifyButtonDisabled] = useState(true); // Magic Button disabled by default
-      const [
-        verifyButtonDisabledDoubleclick,
-        setVerifyButtonDisabledDoubleclick,
-      ] = useState(false); // Magic Button disable on button-click
-
-      const minInputCharsSecurityAnswer = 3;
-      const minInputCharsSMSCode = 8;
-
-      useEffect(() => {
-        if (securityAnswer.length >= minInputCharsSecurityAnswer) {
-          setMagicButtonDisabled(false); // Enable Magic Button if checks are okay
-        } else {
-          setMagicButtonDisabled(true);
-        }
-      }, [magicButtonDisabled, setMagicButtonDisabled, securityAnswer]);
-
-      useEffect(() => {
-        if (key.length >= minInputCharsSMSCode) {
-          setVerifyButtonDisabled(false); // Enable Magic Button if checks are okay
-        } else {
-          setVerifyButtonDisabled(true);
-          setVerifyButtonDisabledDoubleclick(false);
-        }
-      }, [verifyButtonDisabled, setVerifyButtonDisabled, key]);
-
-      if (!payload) return null;
-
-      if (sentMessage) {
-        return (
-          <View
-            style={{
-              flexGrow: 1,
-              flex: 1,
-              paddingHorizontal: 20,
-              justifyContent: "space-between",
-            }}
-          >
-            <View>
-              <Text
-                style={{
-                  color: "#999CB6",
-                  fontSize: 14,
-                  marginTop: 10,
-                }}
-              >
-                <FormattedMessage
-                  id="signature.pasteresponse"
-                  defaultMessage="Paste in the response you received."
-                />
-              </Text>
-              <TextInput
-                placeholder={intl.formatMessage({
-                  id: "signature.smscodelabel",
-                  defaultMessage: "8-Digits SMS-Code",
-                })}
-                textContentType="oneTimeCode"
-                keyboardType="number-pad"
-                style={{ marginTop: 25 }}
-                value={key}
-                onChangeText={setKey}
-                CustomTextInput={BottomSheetTextInput}
-              />
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginTop: 24,
-                }}
-              >
-                <Text
-                  style={{ color: "rgba(246, 245, 255, 0.6)", fontSize: 12 }}
-                >
-                  <FormattedMessage
-                    id="signature.noresponselabel"
-                    defaultMessage="Didn't receive a response?"
-                  />
-                </Text>
-
-                <InlineButton
-                  label={intl.formatMessage({
-                    id: "signature.sendagain",
-                    defaultMessage: "Resend",
-                  })}
-                  onPress={async () => {
-                    const { signDoc } = await getTransactionInformation();
-                    const phoneNumberRequestKey = new PhoneNumberRequestKey({
-                      phoneNumber: payload.phoneNumber,
-                      securityAnswer,
-                      chainId,
-                      wallet,
-                      multisig,
-                    });
-                    await phoneNumberRequestKey.createSignatureAmino(signDoc);
-                  }}
-                />
-              </View>
-            </View>
-
-            <VerifyAndProceedButton
-              onPress={async () => {
-                try {
-                  setVerifyButtonDisabledDoubleclick(true);
-                  const { signDoc } = await getTransactionInformation();
-                  const phoneNumberRequestKey = new PhoneNumberConfirmKey({
-                    key,
-                    wallet,
-                    multisig,
-                  });
-                  const response =
-                    await phoneNumberRequestKey.createSignatureAmino(signDoc);
-                  if (response) onSuccess(response);
-                  setVerifyButtonDisabledDoubleclick(false);
-                } catch (e) {
-                  const error = e as Error;
-                  setVerifyButtonDisabledDoubleclick(false);
-                  console.error(error);
-                  Alert.alert(
-                    intl.formatMessage({
-                      id: "general.error",
-                      defaultMessage: "Error",
-                    }) + "VerifyAndProceedButton (1)",
-                    error.message
-                  );
-                }
-              }}
-              disabled={
-                verifyButtonDisabledDoubleclick
-                  ? verifyButtonDisabledDoubleclick
-                  : verifyButtonDisabled
-              }
-            />
-          </View>
-        );
-      }
-
-      return (
-        <View
-          style={{
-            flexGrow: 1,
-            flex: 1,
-            paddingHorizontal: 20,
-            justifyContent: "space-between",
-          }}
-        >
-          <SecurityQuestionInput
-            disabled
-            securityQuestion={payload.securityQuestion}
-            securityAnswer={securityAnswer}
-            onSecurityAnswerChange={setSecurityAnswer}
-            CustomTextInput={BottomSheetTextInput}
-          />
-
-          <SendMagicSmsButton
-            onPress={async () => {
-              setMagicButtonDisabledDoubleclick(true);
-
-              try {
-                const { signDoc } = await getTransactionInformation();
-                const phoneNumberRequestKey = new PhoneNumberRequestKey({
-                  phoneNumber: payload.phoneNumber,
-                  securityAnswer,
-                  chainId,
-                  wallet,
-                  multisig,
-                });
-                await phoneNumberRequestKey.createSignatureAmino(signDoc);
-                setSentMessage(true);
-                setMagicButtonDisabledDoubleclick(false);
-              } catch (e) {
-                const error = e as Error;
-                setMagicButtonDisabledDoubleclick(false);
-                console.error(error);
-                Alert.alert("Sending SMS failed.", error.message);
-              }
-            }}
-            disabled={
-              magicButtonDisabledDoubleclick
-                ? magicButtonDisabledDoubleclick
-                : magicButtonDisabled
-            }
-          />
-        </View>
-      );
-    }
-  );
