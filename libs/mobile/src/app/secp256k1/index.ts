0636e68e5e24ba4420978f5cb355da6144933a52
Jonas Keinholz <git@inyono.dev>
2023-01-19 16:26:04 +0100

    automatically retry if preparation of wallet fails

--- libs/mobile/src/app/secp256k1/index.ts
+++ libs/mobile/src/app/secp256k1/index.ts
@@ -31,50 +31,64 @@ export async function prepareWalletAndSign({
 
   const { chainStore, configStore } = getRootStore();
 
-  switch (configStore.getDefaultMultisigWalletType()) {
-    case WalletType.CosmosMultisig: {
-      const { chainId, prefix, denom } =
-        chainStore.currentCosmosChainInformation;
-      const client = await createStargateClient(chainId);
+  async function prepareWallet() {
+    switch (configStore.getDefaultMultisigWalletType()) {
+      case WalletType.CosmosMultisig: {
+        const { chainId, prefix, denom } =
+          chainStore.currentCosmosChainInformation;
+        const client = await createStargateClient(chainId);
 
-      const address = pubkeyToAddress(
-        {
-          type: pubkeyType.secp256k1,
-          value: publicKey,
-        },
-        prefix
-      );
-
-      if (!(await client.getAccount(address))) {
-        await lendFees({ chainId, address });
-      }
-
-      // TODO: here we need to wait longer as long as account does not exist
-
-      if (!(await client.getAccount(address))?.pubkey) {
-        const signer = await Secp256k1Wallet.fromKey(
-          privateKeyUint8Array,
+        const address = pubkeyToAddress(
+          {
+            type: pubkeyType.secp256k1,
+            value: publicKey,
+          },
           prefix
         );
-        const signingClient = await createSigningStargateClient({
-          chainId,
-          signer,
-        });
-        await signingClient.sendTokens(
-          address,
-          address,
-          coins(1, denom),
-          "auto",
-          ""
-        );
+
+        if (!(await client.getAccount(address))) {
+          await lendFees({ chainId, address });
+        }
+
+        // TODO: here we need to wait longer as long as account does not exist
+
+        if (!(await client.getAccount(address))?.pubkey) {
+          const signer = await Secp256k1Wallet.fromKey(
+            privateKeyUint8Array,
+            prefix
+          );
+          const signingClient = await createSigningStargateClient({
+            chainId,
+            signer,
+          });
+          await signingClient.sendTokens(
+            address,
+            address,
+            coins(1, denom),
+            "auto",
+            ""
+          );
+        }
+        break;
       }
-      break;
+      case WalletType.TerraMultisig: {
+        const { chainId } = chainStore.currentTerraChainInformation;
+        const key = new RawKey(Buffer.from(privateKeyUint8Array));
+        await terra.prepareKey({ key, chainId });
+        break;
+      }
     }
-    case WalletType.TerraMultisig: {
-      const { chainId } = chainStore.currentTerraChainInformation;
-      const key = new RawKey(Buffer.from(privateKeyUint8Array));
-      await terra.prepareKey({ key, chainId });
+  }
+
+  for (let i = 0; i < 3; i++) {
+    try {
+      await prepareWallet();
       break;
+    } catch (e) {
+      // Throw after last attempt
+      if (i === 2) {
+        throw e;
+      }
     }
   }
 
