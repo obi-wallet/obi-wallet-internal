614e4ec6cab1969b3e76f8f3cde7fba2e85c008f
José Gregorio Andradez <herrkin@Joses-MacBook-Pro-2.local>
2023-01-19 13:46:10 -0300

    fixed modal overflow

--- libs/mobile/src/app/screens/account-picker-modal.tsx
+++ libs/mobile/src/app/screens/account-picker-modal.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@emotion/react";
 import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { isMultisigDemoWallet, Text } from "@obi-wallet/common";
@@ -32,11 +33,13 @@ export function useAccountPickerModalProps() {
 
 export const AccountPickerModal = observer<AccountPickerModalProps>(
   ({ visible, open, close, showNotReadyWallets }) => {
-    const { walletsStore } = useStore();
+    const { walletsStore, configStore } = useStore();
+    const isObi = configStore.isObi()
+    const theme = useTheme()
 
     return (
-      <Modal isVisible={visible} onClose={close}>
-        <View style={{ flexShrink: 1 }}>
+      <Modal isVisible={visible} onClose={close} style={{ backgroundColor: theme.colors.background }}>
+        <View style={{ flexShrink: 1, }}>
           <View>
             <Text
               style={{
@@ -49,7 +52,7 @@ export const AccountPickerModal = observer<AccountPickerModalProps>(
             </Text>
             <Text
               style={{
-                color: "#999CB6",
+                color: isObi ? "#fff" : "#999CB6",
                 fontSize: 14,
                 marginTop: 10,
               }}
@@ -71,7 +74,7 @@ export const AccountPickerModal = observer<AccountPickerModalProps>(
                   style={{
                     height: 79,
                     width: "100%",
-                    backgroundColor: "#111023",
+                    backgroundColor: isObi ? "#272727" : "#111023",
                     marginBottom: 20,
                     flexDirection: "row",
                     borderRadius: 12,
@@ -102,7 +105,7 @@ export const AccountPickerModal = observer<AccountPickerModalProps>(
                     </Text>
                     <Text
                       style={{
-                        color: "#787B9C",
+                        color: isObi ? "#ffffff80" : "#999CB6",
                         fontSize: 14,
                         fontWeight: "600",
                       }}
@@ -138,7 +141,7 @@ export const AccountPickerModal = observer<AccountPickerModalProps>(
                   >
                     <FontAwesomeIcon
                       icon={faTrash}
-                      style={{ color: "#7B87A8" }}
+                      style={{ color: isObi ? "#fff" : "#7B87A8" }}
                     />
                   </IconButton>
                 </TouchableOpacity>
@@ -156,7 +159,7 @@ export const AccountPickerModal = observer<AccountPickerModalProps>(
             onPress={() => close()}
             style={{ paddingVertical: 15 }}
           >
-            <Text style={{ color: "#787B9C" }}>
+            <Text style={{ color: isObi ? "#fff" : "#787B9C" }}>
               <FormattedMessage
                 id="accountpickermodal.close"
                 defaultMessage="Close"
--- libs/mobile/src/app/screens/components/keys-list/index.tsx
+++ libs/mobile/src/app/screens/components/keys-list/index.tsx
@@ -87,7 +87,7 @@ export const KeysList = observer(({ data, style, tiled }: KeysListProps) => {
       style={[
         style,
         isObi
-          ? { borderTopColor: "rgba(255,255,255,.3)", borderTopWidth: 1 }
+          ? { borderTopColor: "rgba(255,255,255,.3)", borderTopWidth: 1, ...(isObi ? { alignItems: 'center' } : {}) }
           : {},
       ]}
     >
@@ -130,16 +130,17 @@ export const KeyListItem = observer(({ item, tiled }: KeyListItemProps) => {
               borderColor: signed && isLoop ? "#89F5C2" : "transparent",
             }}
           >
-            {/* {signed ? */}
-            <LottieView
-              source={require("./assets/confirm-animation.json")}
-              autoPlay
-              style={{ width: 60, zIndex: -1, position: "absolute" }}
-            />
-            {/* : */}
-            {/* <LottieView source={require("./assets/prompt-animation.json")} autoPlay loop style={{ width: 60, zIndex: -1, position: 'absolute' }} /> */}
-            {/* } */}
-            <Icon fill="#7B87A8" />
+            {signed ?
+              <LottieView
+                source={require("./assets/confirm-animation.json")}
+                autoPlay
+                loop={false}
+                style={{ width: 60, zIndex: -1, position: "absolute" }}
+              />
+              :
+              <LottieView source={require("./assets/prompt-animation.json")} autoPlay loop style={{ width: 60, zIndex: -1, position: 'absolute' }} />
+            }
+            <Icon fill={isObi ? "#fff" : "#7B87A8"} />
           </View>
         </View>
         <Text
