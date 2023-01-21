e84553c68289dd2ae392986a13c388769ee651aa
Jonas Keinholz <git@inyono.dev>
2023-01-21 19:24:59 +0100

    common: update phoenix-1 LCD

--- libs/common/src/chains.ts
+++ libs/common/src/chains.ts
@@ -61,7 +61,7 @@ export const terraChains = {
     accountCreatorAddress:
       "terra1a9zykuft0ngvq6ug2j60hz0an2kz72c3vs73tj5m87xcm0dt8w2sdkflln",
     currentCodeId: 1012,
-    lcd: "https://phoenix-lcd.terra.dev",
+    lcd: "https://terra2-api.dalnim.finance/",
     api: "https://phoenix-api.terra.dev",
     denom: "uluna",
     startingUsdDebt: "0",
