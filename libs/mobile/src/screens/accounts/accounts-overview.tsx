e97db42a96b7ff583e88783a35846248a403bf09
Jonas Keinholz <git@inyono.dev>
2023-02-19 16:32:50 -0500

    CreateFlexAccountScreen: basic logic

--- libs/mobile/src/screens/accounts/accounts-overview.tsx
+++ libs/mobile/src/screens/accounts/accounts-overview.tsx
@@ -1,9 +1,11 @@
+import { Text } from "@obi-wallet/common";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { observer } from "mobx-react-lite";
 import { View } from "react-native";
 
 import { AccountsRoute, AccountsStackParamList } from "./accounts-stack";
 import { Button } from "../../app/button";
+import { useMultisigWallet } from "../../app/stores";
 
 export type AccountsOverviewScreenProps = NativeStackScreenProps<
   AccountsStackParamList,
@@ -12,8 +14,13 @@ export type AccountsOverviewScreenProps = NativeStackScreenProps<
 
 export const AccountsOverviewScreen = observer<AccountsOverviewScreenProps>(
   function AccountsOverviewScreen({ navigation }) {
+    const wallet = useMultisigWallet();
+
+    // TODO: accounts should accept draft instead
+
     return (
       <View style={{ marginTop: 100 }}>
+        <Text>{JSON.stringify(wallet.accounts, null, 2)}</Text>
         <Button
           flavor="blue"
           label="Add"
--- libs/mobile/src/screens/accounts/create-flex-account.tsx
+++ libs/mobile/src/screens/accounts/create-flex-account.tsx
@@ -4,6 +4,7 @@ import { View } from "react-native";
 
 import { AccountsRoute, AccountsStackParamList } from "./accounts-stack";
 import { Button } from "../../app/button";
+import { useMultisigWallet } from "../../app/stores";
 
 export type CreateFlexAccountScreenProps = NativeStackScreenProps<
   AccountsStackParamList,
@@ -12,12 +13,32 @@ export type CreateFlexAccountScreenProps = NativeStackScreenProps<
 
 export const CreateFlexAccountScreen = observer<CreateFlexAccountScreenProps>(
   function CreateFlexAccountScreen({ navigation }) {
+    const wallet = useMultisigWallet();
+
     return (
       <View style={{ marginTop: 100 }}>
         <Button
           flavor="blue"
           label="Create Flex Account"
           onPress={() => {
+            // TODO: use draft instead
+            wallet.gatekeeperConfig.flexAccounts.add({
+              entity: {
+                meta: {
+                  icon: "",
+                  name: "name",
+                },
+                address: "0x123",
+                autoSign: null,
+                spendLimit: null,
+                privateKey: "123",
+                publicKey: {
+                  type: "tendermint/PubKeySecp256k1",
+                  value: "123",
+                },
+              },
+            });
+
             navigation.navigate(AccountsRoute.AccountsOverview);
           }}
         />
