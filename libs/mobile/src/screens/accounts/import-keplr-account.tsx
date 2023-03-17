019449bf56b117104e9b82c4a721a530ad240dca
José Gregorio Andradez <herrkin@Joses-MacBook-Pro.local>
2023-03-17 15:53:26 -0300

    ADD validation to legacy form

--- libs/mobile/src/screens/accounts/import-keplr-account.tsx
+++ libs/mobile/src/screens/accounts/import-keplr-account.tsx
@@ -1,16 +1,9 @@
 import { Text } from "@obi-wallet/common";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { MnemonicKey } from "@terra-money/feather.js";
 import { observer } from "mobx-react-lite";
-import { useState } from "react";
-import { View } from "react-native";
-import invariant from "tiny-invariant";
 
 import { AccountsRoute, AccountsStackParamList } from "./accounts-stack";
-import { Button } from "../../app/button";
-import { ScreenContainer } from "../../app/screens/components/screen-container";
-import { useMultisigWallet } from "../../app/stores";
-import { TextInput } from "../../app/text-input";
+import { LegacyForm } from "./legacy-form";
 
 export type ImportKeplrAccountScreenProps = NativeStackScreenProps<
   AccountsStackParamList,
@@ -18,64 +11,7 @@ export type ImportKeplrAccountScreenProps = NativeStackScreenProps<
 >;
 
 export const ImportKeplrAccountScreen = observer<ImportKeplrAccountScreenProps>(
-  function ImportKeplrAccountScreen({ navigation }) {
-    const wallet = useMultisigWallet();
-    const [mnemonic, setMnemonic] = useState("");
-
-    return (
-      <ScreenContainer>
-        <View
-          style={{
-            flex: 1,
-            alignItems: "center",
-            marginTop: 20,
-          }}
-        >
-          <Text style={{ color: "white", fontSize: 16, marginBottom: 15 }}>
-            Import Legacy Account
-          </Text>
-          <TextInput
-            placeholder="Enter Seedphrase"
-            label="Seedphrase"
-            style={{ width: "100%", marginTop: 40 }}
-            value={mnemonic}
-            onChangeText={setMnemonic}
-          />
-        </View>
-        <View style={{ marginTop: 20 }}>
-          <Button
-            flavor="blue"
-            onPress={() => {
-              const key = new MnemonicKey({
-                mnemonic,
-                coinType: 118,
-              });
-
-              const publicKey = key.publicKey?.toAmino();
-              invariant(
-                publicKey && publicKey.type === "tendermint/PubKeySecp256k1",
-                'Expected key to be of type "tendermint/PubKeySecp256k1"'
-              );
-
-              wallet.upsertSinglesigWallet({
-                type: "singlesig-wallet",
-                publicKey,
-                privateKey: key.privateKey.toString("base64"),
-              });
-
-              navigation.navigate(AccountsRoute.AccountsOverview);
-            }}
-            label="Confirm"
-          />
-          <Button
-            flavor="cancel"
-            onPress={() => {
-              navigation.goBack();
-            }}
-            label="Cancel"
-          />
-        </View>
-      </ScreenContainer>
-    );
+  function ImportKeplrAccountScreen(props) {
+    return <LegacyForm {...props} />;
   }
 );
---
+++ libs/mobile/src/screens/accounts/legacy-form.tsx
@@ -0,0 +1,97 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Text } from "@obi-wallet/common";
+import { MnemonicKey } from "@terra-money/feather.js";
+import { observer } from "mobx-react-lite";
+import { Controller, useForm } from "react-hook-form";
+import { View } from "react-native";
+import invariant from "tiny-invariant";
+import { z } from "zod";
+
+import { AccountsRoute } from "./accounts-stack";
+import { ImportKeplrAccountScreenProps } from "./import-keplr-account";
+import { ImportStationAccountScreenProps } from "./import-station-account";
+import { Button } from "../../app/button";
+import { ScreenContainer } from "../../app/screens/components/screen-container";
+import { useMultisigWallet } from "../../app/stores";
+import { TextInput } from "../../app/text-input";
+import { validateMnemonic } from "../../helpers/validation-helpers";
+
+export const LegacyForm = observer<
+  ImportKeplrAccountScreenProps | ImportStationAccountScreenProps
+>(function LegacyForm({ navigation }) {
+  const wallet = useMultisigWallet();
+  const schema = z.object({
+    mnemonic: validateMnemonic(),
+  });
+  const { control, handleSubmit, formState } = useForm({
+    defaultValues: {
+      mnemonic: "",
+    },
+    mode: "onTouched",
+    resolver: zodResolver(schema),
+  });
+  return (
+    <ScreenContainer>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          marginTop: 20,
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 16, marginBottom: 15 }}>
+          Import Legacy Account
+        </Text>
+        <Controller
+          name="mnemonic"
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="Enter Seedphrase"
+              label="Seedphrase"
+              style={{ width: "100%", marginTop: 40 }}
+              value={value}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              invalidMessage={formState.errors.mnemonic?.message}
+            />
+          )}
+        />
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <Button
+          flavor="blue"
+          disabled={!formState.isValid}
+          onPress={handleSubmit((data) => {
+            const key = new MnemonicKey({
+              mnemonic: data.mnemonic,
+              coinType: 118,
+            });
+
+            const publicKey = key.publicKey?.toAmino();
+            invariant(
+              publicKey && publicKey.type === "tendermint/PubKeySecp256k1",
+              'Expected key to be of type "tendermint/PubKeySecp256k1"'
+            );
+
+            wallet.addSinglesigWallet({
+              type: "singlesig-wallet",
+              publicKey,
+              privateKey: key.privateKey.toString("base64"),
+            });
+
+            navigation.navigate(AccountsRoute.AccountsOverview);
+          })}
+          label="Confirm"
+        />
+        <Button
+          flavor="cancel"
+          onPress={() => {
+            navigation.goBack();
+          }}
+          label="Cancel"
+        />
+      </View>
+    </ScreenContainer>
+  );
+});
