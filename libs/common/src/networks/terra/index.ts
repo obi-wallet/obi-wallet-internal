089cb65703c5c49a0d9fce27b4c4f4c04b720e91
Jonas Keinholz <git@inyono.dev>
2023-04-02 16:27:00 +0200

    clean up

--- libs/common/src/networks/terra/index.ts
+++ libs/common/src/networks/terra/index.ts
@@ -1,11 +1,7 @@
-import * as helpers from "./helpers";
 import * as messages from "./messages";
 import * as tokens from "./tokens";
-import * as userAccount from "./user-account";
 
 export const terra = {
-  ...helpers,
   ...messages,
-  ...userAccount,
   ...tokens,
 };
