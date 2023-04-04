1120f3b5e60dddc97016ad2b8a623728dc90907d
José Gregorio Andradez <herrkin@Joses-MacBook-Pro.local>
2023-04-04 12:03:53 -0400

    Android can promt if you sent the mail to yourself

--- libs/mobile/src/screens/keys/email/index.tsx
+++ libs/mobile/src/screens/keys/email/index.tsx
@@ -1,13 +1,18 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Text } from "@obi-wallet/common";
-import { generateSec256k1KeyPair, MultisigKey } from "@obi-wallet/sdk";
+import {
+  generateSec256k1KeyPair,
+  MultisigKey,
+  Secp256k1PublicKey,
+} from "@obi-wallet/sdk";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { observer } from "mobx-react-lite";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { FormattedMessage, useIntl } from "react-intl";
-import { Alert, Linking, TouchableOpacity, View } from "react-native";
+import { Alert, Linking, TouchableOpacity, View, AppState } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import useAppState from "react-native-useappstate";
 import { z } from "zod";
 
 import {
@@ -82,6 +87,34 @@ export const EmailKey = observer<EmailKeyProps>(function EmailKey({
   const [selectedTab, setSelectedTab] = useState(Tab.EmailKeyV1);
   const isObi = configStore.isObi();
   const intl = useIntl();
+
+  const [emailKey, setEmailKey] = useState<Secp256k1PublicKey | undefined>();
+  const appState = useAppState();
+
+  useEffect(() => {
+    if (appState === "active" && emailKey) {
+      Alert.alert(
+        "Confirm Email Sent",
+        "Never enter the one-time key you received anywhere unless you need it for recovery. Have you sent the email to yourself?",
+        [
+          {
+            text: "No",
+            style: "cancel",
+          },
+          {
+            text: "Yes, I sent the email to myself",
+            onPress: () => {
+              if (emailKey) {
+                draft.value.setEmailKey(emailKey);
+                onSubmit();
+              }
+            },
+          },
+        ],
+        { cancelable: false }
+      );
+    }
+  }, [appState]);
 
   const isKeyboardVisible = useKeyboardVisible();
 
@@ -285,36 +318,18 @@ export const EmailKey = observer<EmailKeyProps>(function EmailKey({
                 onPress={handleSubmit(async (data) => {
                   try {
                     const { publicKey, privateKey } = generateSec256k1KeyPair();
+                    const URL = `mailto:${
+                      data.email
+                    }?subject=Obi%20DO%20NOT%20DELETE:%20Recovery%20Assistant&body=${encodeForMailto(
+                      "This is a v1 recovery key. You are sending it to yourself; Obi can never access its contents. " +
+                        "This key is one-time use and can be used to help you recover if you lose multiple factors. " +
+                        "DO NOT DELETE this email unless you are saving its contents to a password manager or physical location. In future versions " +
+                        "of Obi, email recovery will use zero-knowledge proofs, and so saving an email will be unnecessary.  " +
+                        privateKey
+                    )}`;
 
-                    await Linking.openURL(
-                      `mailto:${
-                        data.email
-                      }?subject=Obi%20DO%20NOT%20DELETE:%20Recovery%20Assistant&body=${encodeForMailto(
-                        "This is a v1 recovery key. You are sending it to yourself; Obi can never access its contents. " +
-                          "This key is one-time use and can be used to help you recover if you lose multiple factors. " +
-                          "DO NOT DELETE this email unless you are saving its contents to a password manager or physical location. In future versions " +
-                          "of Obi, email recovery will use zero-knowledge proofs, and so saving an email will be unnecessary.  " +
-                          privateKey
-                      )}`
-                    );
-                    Alert.alert(
-                      "Confirm Email Sent",
-                      "Never enter the one-time key you received anywhere unless you need it for recovery. Have you sent the email to yourself?",
-                      [
-                        {
-                          text: "No",
-                          style: "cancel",
-                        },
-                        {
-                          text: "Yes, I sent the email to myself",
-                          onPress: () => {
-                            draft.value.setEmailKey(publicKey);
-                            onSubmit();
-                          },
-                        },
-                      ],
-                      { cancelable: false }
-                    );
+                    setEmailKey(publicKey);
+                    await Linking.openURL(URL);
                   } catch (e) {
                     console.error(e);
                     // noop
