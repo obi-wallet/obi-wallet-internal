d2215a1762a70ca7cc210d93ca4f36b3d72b8155
Jonas Keinholz <git@inyono.dev>
2023-05-09 16:14:27 +0200

    release 0.14.1 v71

--- apps/obi-mobile/src/main.tsx
+++ apps/obi-mobile/src/main.tsx
@@ -4,7 +4,7 @@ import { createApp } from "./app";
 
 const appName = "money.obi.wallet";
 const version = "0.14.1";
-const codepushVersion = "70";
+const codepushVersion = "71";
 const release = `${appName}@${version}+codepush:v${codepushVersion}`;
 
 setupMain({
