0170f6eb24432ba7358bc5d6d0ba41409296e911
Jonas Keinholz <git@inyono.dev>
2023-05-26 12:51:41 +0200

    make KeysList scrollable

--- libs/common/src/components/multisig-settings/keys-list.tsx
+++ libs/common/src/components/multisig-settings/keys-list.tsx
@@ -64,7 +64,7 @@ export const KeysList = observer(function KeysList({
   });
 
   return (
-    <View style={[style]}>
+    <View style={[{ flex: 1 }, style]}>
       <FlatList
         data={[
           ...hydratedData,
