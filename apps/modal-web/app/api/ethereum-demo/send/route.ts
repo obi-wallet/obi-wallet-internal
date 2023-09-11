93057c1959331cee68a1a5fa4e62580f1dc10eb5
rustopian <pete@obi.money>
2023-09-11 17:03:59 -0400

    fix type strictness in eth send route

--- apps/modal-web/app/api/ethereum-demo/send/route.ts
+++ apps/modal-web/app/api/ethereum-demo/send/route.ts
@@ -71,9 +71,11 @@ export async function POST(request: Request) {
   );
   const client = await Client.init(config.rpcUrl!);
   const amount = parseUnits(body.token.rawAmount, 0);
-  let deviceKey;
+  let signingKey, _;
   if (!homeChain.zAuthKeyPair) {
-    deviceKey = await getOrCreateDeviceKeyPair(true, false, false);
+    [signingKey, _] = await getOrCreateDeviceKeyPair(true, false, false);
+  } else {
+    signingKey = homeChain.zAuthKeyPair;
   }
   const signer = new SecretJsSigner(
     {
@@ -82,7 +84,7 @@ export async function POST(request: Request) {
       proxyAddress: homeChain.proxyAddress,
       targetChain: homeChain.targetChain,
     },
-    deviceKey,
+    signingKey,
   );
   const simpleAccount = await Presets.Builder.SimpleAccount.init(
     // @ts-expect-error this should be fine
