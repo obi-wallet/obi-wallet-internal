51d74e01fcdaeba7d4f1f2e3349d7bce10139ac9
rustopian <pete@obi.money>
2023-09-11 14:37:16 -0400

    evm for device key

--- apps/modal-web/app/api/zauth/create-account/route.ts
+++ apps/modal-web/app/api/zauth/create-account/route.ts
@@ -1,22 +1,17 @@
 import { EthereumAccount } from "@obi-wallet/headless-ui";
 import {
-  KeyType,
-  Messages,
-  MultisigKey,
+  Secp256k1KeyPair,
   SecretJsChainId,
-  SecretJsClient,
   generateSec256k1KeyPair,
 } from "@obi-wallet/sdk";
 import { NextResponse } from "next/server";
-import { TxResponse } from "secretjs";
-import invariant from "tiny-invariant";
 
 // import { cookies } from "next/headers";
 import { connect } from "../../../../src/db";
 import { HomeChain } from "../../../../src/db/schema";
-import { getFeeLender } from "../../../../src/fee-lender";
 import {
   generateEthereumAccount,
+  generateEthereumAddress,
   recoverEthereumAccount,
 } from "../../../../src/stackup";
 import { fetchUserId } from "../../../../src/zauth";
@@ -26,6 +21,7 @@ export async function POST(request: Request) {
     accessToken?: string;
     refreshToken?: string;
     homeChainId: SecretJsChainId;
+    deviceKeypair: Secp256k1KeyPair;
   } = await request.json();
 
   const accessToken = body.accessToken;
@@ -34,159 +30,181 @@ export async function POST(request: Request) {
   const refreshToken = body.accessToken;
   // ?? cookies().get("refreshToken")?.value;
 
-  const userId = accessToken ? await fetchUserId(accessToken) : null;
-  if (!accessToken || !refreshToken || !userId) {
-    return NextResponse.json(
-      {
-        error: "invalid token",
-      },
-      { status: 401 },
-    );
-  }
-
-  async function fetchOrCreateUser() {
-    const UserModel = await connect();
-    const existingUser = await UserModel.findOne({ userId });
-
-    if (existingUser) {
-      const homeChain = existingUser.homeChains.get(body.homeChainId);
-      if (homeChain) {
-        const keyPair = homeChain.zAuthKeyPair;
-        const ethereumAccount = await recoverEthereumAccount({
-          chainId: body.homeChainId,
-          zAuthKeyPair: homeChain.zAuthKeyPair,
-          proxyAddress: homeChain.proxyAddress,
-          targetChain: homeChain.targetChain,
-        });
-        return {
-          newUser: false,
-          publicKey: keyPair.publicKey,
-          proxyAddress: homeChain.proxyAddress,
-          ethereumAccount,
-        };
-      }
-    }
-
-    const keyPair = generateSec256k1KeyPair();
-
-    const messagesSdk = Messages.chainId(body.homeChainId);
-    const client = new SecretJsClient(body.homeChainId);
-    const { wallet, signer } = getFeeLender(body.homeChainId);
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    async function generateProxyAddress() {
-      const multisigKey = MultisigKey.create(body.homeChainId, {
-        keys: [
-          {
-            type: KeyType.ZAuth,
-            payload: {
-              publicKey: keyPair.publicKey,
-            },
-          },
-        ],
-        threshold: 1,
-      });
-      const message = messagesSdk.getCreateWalletMessage(multisigKey);
-      (message as { sender: string }).sender = wallet.address;
-      const signedTransaction = await client.createAndSignTransaction({
-        signer,
-        messages: [message],
-      });
-      const broadcastTransactionResult =
-        await client.broadcastSignedTransaction(signedTransaction);
-      console.log(broadcastTransactionResult);
-
-      if (!broadcastTransactionResult.success) {
-        return {
-          approved: true,
-          payload: {
-            success: false,
-            description: "Transaction failed",
-            originalPayload: broadcastTransactionResult,
-          },
-        };
-      }
-
-      const response = broadcastTransactionResult.rawResult as TxResponse;
-      const contractAddress = response.arrayLog?.find((log) => {
-        return log.type === "instantiate" && log.key === "contract_address";
-      })?.value;
-      invariant(contractAddress, "Contract address not found");
-      return contractAddress;
-    }
-
-    async function missing(): Promise<string> {
-      return "MISSING";
-    }
-
-    const [proxyAddress, ethereumAccount] = await Promise.all([
-      // generateProxyAddress()
-      missing(),
-      generateEthereumAccount({
-        chainId: body.homeChainId,
-        keyPair,
-      }),
-    ] as [Promise<string>, Promise<EthereumAccount>]);
-
-    const homeChain: HomeChain = {
-      zAuthKeyPair: keyPair,
-      targetChain: {
-        publicKey: ethereumAccount.publicKey,
-        evmAddress: ethereumAccount.address,
-      },
-      proxyAddress,
-    };
-
-    if (existingUser) {
-      existingUser.homeChains.set(body.homeChainId, homeChain);
-      existingUser.save();
-    } else {
-      await UserModel.create({
-        userId,
-        homeChains: {
-          [body.homeChainId]: homeChain,
+  if (accessToken && refreshToken) {
+    const userId = accessToken ? await fetchUserId(accessToken) : null;
+    if (!accessToken || !refreshToken || !userId) {
+      return NextResponse.json(
+        {
+          error: "invalid token",
         },
-      });
+        { status: 401 },
+      );
     }
 
+    const user = await fetchOrCreateZauthUser();
+    return NextResponse.json(user);
+  } else {
+    const user = await fetchOrCreateDeviceUser();
+    return NextResponse.json(user);
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/*
+async function generateProxyAddress(
+  chainId: chainId,
+  keyPair: Secp256k1KeyPair
+) {
+  const messagesSdk = Messages.chainId(chainId);
+  const client = new SecretJsClient(chainId);
+  const { wallet, signer } = getFeeLender(chainId);
+
+  const multisigKey = MultisigKey.create(chainId, {
+    keys: [
+      {
+        type: KeyType.ZAuth,
+        payload: {
+          publicKey: keyPair.publicKey,
+        },
+      },
+    ],
+    threshold: 1,
+  });
+  const message = messagesSdk.getCreateWalletMessage(multisigKey);
+  (message as { sender: string }).sender = wallet.address;
+  const signedTransaction = await client.createAndSignTransaction({
+    signer,
+    messages: [message],
+  });
+  const broadcastTransactionResult =
+    await client.broadcastSignedTransaction(signedTransaction);
+  console.log(broadcastTransactionResult);
+
+  if (!broadcastTransactionResult.success) {
     return {
-      newUser: true,
-      publicKey: keyPair.publicKey,
-      proxyAddress,
-      ethereumAccount,
+      approved: true,
+      payload: {
+        success: false,
+        description: "Transaction failed",
+        originalPayload: broadcastTransactionResult,
+      },
     };
   }
 
-  const user = await fetchOrCreateUser();
+  const response = broadcastTransactionResult.rawResult as TxResponse;
+  const contractAddress = response.arrayLog?.find((log) => {
+    return log.type === "instantiate" && log.key === "contract_address";
+  })?.value;
+  invariant(contractAddress, "Contract address not found");
+  return contractAddress;
+}
+*/
+
+async function missing(): Promise<string> {
+  return "MISSING";
+}
+
+async function fetchOrCreateZauthUser() {
+  const UserModel = await connect();
+  const existingUser = await UserModel.findOne({ userId });
+
+  if (existingUser) {
+    const homeChain = existingUser.homeChains.get(body.homeChainId);
+    if (homeChain) {
+      const keyPair = homeChain.zAuthKeyPair;
+      const ethereumAccount = await recoverEthereumAccount({
+        chainId: body.homeChainId,
+        zAuthKeyPair: homeChain.zAuthKeyPair,
+        proxyAddress: homeChain.proxyAddress,
+        targetChain: homeChain.targetChain,
+      });
+      return {
+        newUser: false,
+        publicKey: keyPair.publicKey,
+        proxyAddress: homeChain.proxyAddress,
+        ethereumAccount,
+      };
+    }
+  }
+
+  const keyPair = generateSec256k1KeyPair();
+
+  const [proxyAddress, ethereumAccount] = await Promise.all([
+    // generateProxyAddress()
+    missing(),
+    generateEthereumAccount({
+      chainId: body.homeChainId,
+      keyPair,
+    }),
+  ] as [Promise<string>, Promise<EthereumAccount>]);
+
+  const homeChain: HomeChain = {
+    zAuthKeyPair: keyPair,
+    targetChain: {
+      publicKey: ethereumAccount.publicKey,
+      evmAddress: ethereumAccount.address,
+    },
+    proxyAddress,
+  };
+
+  if (existingUser) {
+    existingUser.homeChains.set(body.homeChainId, homeChain);
+    existingUser.save();
+  } else {
+    await UserModel.create({
+      userId,
+      homeChains: {
+        [body.homeChainId]: homeChain,
+      },
+    });
+  }
+
+  return {
+    newUser: true,
+    publicKey: keyPair.publicKey,
+    proxyAddress,
+    ethereumAccount,
+  };
+}
+
+async function fetchOrCreateDeviceUser() {
+  // try to recover Ethereum account from devicePubkey
+  const chainId = "secret-4";
+  const ethereumAccount = {
+    chainId: chainId,
+    zAuthKeyPair: body.deviceKeypair,
+    proxyAddress: "MISSING",
+    targetChain: {
+      publicKey: body.deviceKeypair.publicKey,
+      evmAddress: await generateEthereumAddress(body.deviceKeypair),
+    },
+  };
+  return {
+    newUser: true,
+    publicKey: body.deviceKeypair.publicKey,
+    proxyAddress: "MISSING",
+    ethereumAccount: ethereumAccount,
+  };
 
   /*
-  cookies().set({
-    name: "zepetoAccessToken",
-    value: accessToken,
-    httpOnly: true,
-    maxAge: 3600000,
-    path: "/",
-    ...(process.env.NODE_ENV === "production"
-      ? {
-          sameSite: "none",
-          secure: true,
-        }
-      : {}),
-  });
-  cookies().set({
-    name: "zepetoRefreshToken",
-    value: refreshToken,
-    httpOnly: true,
-    maxAge: 3600000,
-    path: "/",
-    ...(process.env.NODE_ENV === "production"
-      ? {
-          sameSite: "none",
-          secure: true,
-        }
-      : {}),
-  });
-  */
+  const keyPair = generateSec256k1KeyPair();
 
-  return NextResponse.json(user);
+  const [proxyAddress, ethereumAccount] = await Promise.all([
+    // generateProxyAddress()
+    missing(),
+    generateEthereumAccount({
+      chainId: body.homeChainId,
+      keyPair,
+    }),
+  ] as [Promise<string>, Promise<EthereumAccount>]);
+
+  const homeChain: HomeChain = {
+    zAuthKeyPair: keyPair,
+    targetChain: {
+      publicKey: ethereumAccount.publicKey,
+      evmAddress: ethereumAccount.address,
+    },
+    proxyAddress,
+  };
+  */
 }
