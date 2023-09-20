5b5c819f1e26404590a68dd13064e72c88e4139d
icnawi <icnawi@outlook.com>
2023-09-20 15:22:16 -0700

    Update next.js version with memory fixes

--- apps/modal-web/next.config.js
+++ apps/modal-web/next.config.js
@@ -8,9 +8,6 @@ const { DefinePlugin } = require("webpack");
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  experimental: {
-    appDir: true
-  },
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
