d8eaf0854194f21720d3aeae0b957bc3b98f6c7c
Jonas Keinholz <git@inyono.dev>
2023-12-14 13:25:01 +0100

    remove mobile

--- deprecated/libs/mobile/src/helpers/invariant.ts
+++
@@ -1,8 +0,0 @@
-import invariant from "tiny-invariant";
-
-export function envInvariant(name: string, variable: unknown) {
-  invariant(
-    variable,
-    `Environment variable \`${name}\` is not defined. Is your \`.env\` file up-to-date?`,
-  );
-}
