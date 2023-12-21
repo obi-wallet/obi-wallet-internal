4fc96c423b09ab5c81dae11d02a98050bae8ac5b
Napster <napster0125@gmail.com>
2023-12-21 13:22:41 -0600

    removed comment

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,5 @@
 import { PrimaryLink, Text } from "@/components";
 
-// import wizardImage from "~/assets/images/Obi Wizard.png";
 export default function Homepage() {
   return (
     <section className="flex w-full flex-col items-center justify-center space-y-9">
