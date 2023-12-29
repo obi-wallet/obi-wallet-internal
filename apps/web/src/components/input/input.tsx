cb1bdcdcb6600fa8a349974d78b2726b94d2d069
Napster <napster0125@gmail.com>
2023-12-29 06:24:28 -0600

    refactored the input component

--- apps/web/src/components/input/input.tsx
+++ apps/web/src/components/input/input.tsx
@@ -7,13 +7,25 @@ import {
   useEffect,
   useState,
 } from "react";
+import { IconType } from "react-icons";
 
-type InputProps = ComponentPropsWithoutRef<"input">;
+type InputProps = {
+  startIcon?: IconType;
+  endIcon?: IconType;
+  classNames?: {
+    startIcon?: string;
+    endIcon?: string;
+  };
+} & ComponentPropsWithoutRef<"input">;
 
 export function Input({
   onChange,
   className,
   defaultValue,
+  startIcon: StartIcon,
+  endIcon: EndIcon,
+  classNames,
+  disabled,
   ...rest
 }: InputProps) {
   const [text, setText] = useState("");
@@ -28,18 +40,47 @@ export function Input({
   };
 
   return (
-    <input
-      type="text"
-      name="title"
-      id="title"
-      className={cn(
-        "block w-full rounded-md border border-zinc-800 bg-slate-950  px-8 py-2 font-normal text-white focus:border-gray-400",
-        className,
+    <div className="relative">
+      {StartIcon && (
+        <span className="absolute left-3 top-1/2 -translate-y-1/2">
+          <StartIcon
+            width={16}
+            height={16}
+            className={cn(
+              "text-white",
+              disabled && "opacity-50",
+              classNames?.startIcon,
+            )}
+          />
+        </span>
       )}
-      required
-      value={text}
-      onChange={handleChange}
-      {...rest}
-    />
+      <input
+        type="text"
+        name="title"
+        id="title"
+        className={cn(
+          "block w-full rounded-md border border-zinc-800 bg-slate-950 px-8 py-2 font-normal text-white focus:border-blue-600 focus-visible:outline-none",
+          className,
+        )}
+        required
+        value={text}
+        onChange={handleChange}
+        {...rest}
+        disabled={disabled}
+      />
+      {EndIcon && (
+        <span className="absolute right-3 top-1/2 -translate-y-1/2">
+          <EndIcon
+            width={16}
+            height={16}
+            className={cn(
+              "text-white",
+              disabled && "opacity-50",
+              classNames?.endIcon,
+            )}
+          />
+        </span>
+      )}
+    </div>
   );
 }
