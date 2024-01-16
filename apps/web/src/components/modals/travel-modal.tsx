1c7a3decb898dfa7b49595e82de83d0ba8eeb3ea
Jonas Keinholz <git@inyono.dev>
2024-01-16 16:24:12 +0100

    basic login modal

--- apps/web/src/components/modals/travel-modal.tsx
+++ apps/web/src/components/modals/travel-modal.tsx
@@ -6,9 +6,9 @@ import {
   BalanceInput,
   Box,
   Button,
-  Divider,
   IBalanceOption,
   Input,
+  Modal,
   Text,
 } from "..";
 
@@ -20,41 +20,37 @@ export function TravelModal({ balances }: { balances: IBalanceOption[] }) {
   ];
 
   return (
-    <div className="absolute top-0 flex h-full w-full items-center justify-center rounded-md bg-black/30 backdrop-blur-sm">
-      <Box className="w-[560px] space-y-4 pt-6 shadow-lg shadow-neutral-600">
-        <Text size="xl">Obi Fast Travel</Text>
-        <Divider />
-        <BalanceInput
-          showMaxButton={false}
-          balances={balances}
-          placeholder="Deposit"
-        />
-        <Input placeholder="Receive (Estimated)" endIcon={FaSketch} />
+    <Modal title="Obi Fast Travel">
+      <BalanceInput
+        showMaxButton={false}
+        balances={balances}
+        placeholder="Deposit"
+      />
+      <Input placeholder="Receive (Estimated)" endIcon={FaSketch} />
 
-        <div className="space-y-2">
-          <Text color="zinc" size="xs">
-            Slippage Tolerance
-          </Text>
+      <div className="space-y-2">
+        <Text color="zinc" size="xs">
+          Slippage Tolerance
+        </Text>
 
-          <div className="flex flex-row space-x-3">
-            {tolerances.map((tolerance) => (
-              <Box
-                key={`asset-${tolerance.label}`}
-                className={cn(
-                  "flex flex-row space-x-3 ",
-                  tolerance.selected ? "bg-slate-950" : "bg-gray-700",
-                )}
-              >
-                <Text>{tolerance.label}</Text>
-              </Box>
-            ))}
-          </div>
+        <div className="flex flex-row space-x-3">
+          {tolerances.map((tolerance) => (
+            <Box
+              key={`asset-${tolerance.label}`}
+              className={cn(
+                "flex flex-row space-x-3 ",
+                tolerance.selected ? "bg-slate-950" : "bg-gray-700",
+              )}
+            >
+              <Text>{tolerance.label}</Text>
+            </Box>
+          ))}
         </div>
+      </div>
 
-        <div className="mt-8 flex justify-end">
-          <Button className="block w-44">Execute</Button>
-        </div>
-      </Box>
-    </div>
+      <div className="mt-8 flex justify-end">
+        <Button className="block w-44">Execute</Button>
+      </div>
+    </Modal>
   );
 }
