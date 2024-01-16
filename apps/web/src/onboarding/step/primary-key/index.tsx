f7f407d4f1872f4cb594550f641d00ff4e34ff60
Jonas Keinholz <git@inyono.dev>
2024-01-16 15:33:40 +0100

    hide alternative services when creating primary key

--- apps/web/src/onboarding/step/primary-key/index.tsx
+++ apps/web/src/onboarding/step/primary-key/index.tsx
@@ -6,7 +6,6 @@ import { StepProps } from "@/onboarding/step";
 import { PasskeyStep } from "@/onboarding/step/pass-key";
 import { observer } from "mobx-react-lite";
 import { useState } from "react";
-import { FaApple, FaGoogle, FaWindows } from "react-icons/fa";
 
 enum Choice {
   Passkey,
@@ -58,28 +57,33 @@ export const PrimaryKeyStep = observer(function PrimaryKeyStep({
             variant="primary"
           >
             <div>Passkey</div>
-            <div>(Recommended)</div>
+            {/* TODO: recommendation only makes sense when we have multiple options */}
+            {/*<div>(Recommended)</div>*/}
+          </Button>
+          <Button disabled className="block w-full" variant="primary">
+            More Services Coming Soon
           </Button>
 
-          <div className="flex w-full items-center">
-            <div className="h-0.5 w-full rounded-lg bg-gray-600" />
-            <Text className="grow-0 px-3" color="gray">
-              OR
-            </Text>
-            <div className="h-0.5 w-full rounded-lg bg-gray-600" />
-          </div>
+          {/* TODO: cloud keys aren't integrated yet */}
+          {/*<div className="flex w-full items-center">*/}
+          {/*  <div className="h-0.5 w-full rounded-lg bg-gray-600" />*/}
+          {/*  <Text className="grow-0 px-3" color="gray">*/}
+          {/*    OR*/}
+          {/*  </Text>*/}
+          {/*  <div className="h-0.5 w-full rounded-lg bg-gray-600" />*/}
+          {/*</div>*/}
 
-          <div className="flex w-full flex-row justify-around">
-            <div className="flex h-16 w-16 items-center justify-center rounded-full bg-zinc-800">
-              <FaApple className="h-9 w-9 text-white" />
-            </div>
-            <div className="flex h-16 w-16 items-center justify-center rounded-full bg-zinc-800">
-              <FaGoogle className="h-7 w-7 text-white" />
-            </div>
-            <div className="flex h-16 w-16 items-center justify-center rounded-full bg-zinc-800">
-              <FaWindows className="h-7 w-7 text-white" />
-            </div>
-          </div>
+          {/*<div className="flex w-full flex-row justify-around">*/}
+          {/*  <div className="flex h-16 w-16 items-center justify-center rounded-full bg-zinc-800">*/}
+          {/*    <FaApple className="h-9 w-9 text-white" />*/}
+          {/*  </div>*/}
+          {/*  <div className="flex h-16 w-16 items-center justify-center rounded-full bg-zinc-800">*/}
+          {/*    <FaGoogle className="h-7 w-7 text-white" />*/}
+          {/*  </div>*/}
+          {/*  <div className="flex h-16 w-16 items-center justify-center rounded-full bg-zinc-800">*/}
+          {/*    <FaWindows className="h-7 w-7 text-white" />*/}
+          {/*  </div>*/}
+          {/*</div>*/}
 
           {back ? (
             <Button onClick={back} className="block w-full" variant="outline">
