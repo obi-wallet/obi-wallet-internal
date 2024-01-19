29b73a9e2799df5792e4b78aab5a1462cfc1b042
Napster <napster0125@gmail.com>
2024-01-18 18:13:27 -0600

    refactored the notification component

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components";
+import { Header, Notification } from "@/components";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import dynamic from "next/dynamic";
@@ -19,6 +19,8 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  // TODO: get this value from Jonas's implementation
+  const isOnlyPassKey = true;
   return (
     <html lang="en" className="bg-black">
       <body
@@ -29,6 +31,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       >
         <Provider>
           <Header />
+          {isOnlyPassKey && (
+            <Notification
+              description="CAUTION: Your account is currently only secured by your device passkey. Please add a <b>mobile key</b> to enable recovery on other devices."
+              type="warning"
+            />
+          )}
           <main id="main" className="relative flex w-full grow">
             {children}
           </main>
