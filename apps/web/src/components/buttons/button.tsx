7d4f373e7d9f3e74dd1ccfa22c8d085841cd7e95
napster <napster0125@gmail.com>
2024-01-22 13:05:39 -0600

    button + button link with href

--- apps/web/src/components/buttons/button.tsx
+++ apps/web/src/components/buttons/button.tsx
@@ -1,9 +1,10 @@
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 import { ComponentPropsWithRef, forwardRef } from "react";
 import { IconType } from "react-icons";
 import { FaArrowsRotate } from "react-icons/fa6";
 
-const ButtonVariant = ["primary", "outline", "confirmed"] as const;
+const ButtonVariant = ["primary", "outline", "confirmed", "secondary"] as const;
 const ButtonSize = ["sm", "base"] as const;
 
 type ButtonProps = {
@@ -18,7 +19,9 @@ type ButtonProps = {
     rightIcon?: string;
   };
   block?: boolean;
-} & ComponentPropsWithRef<"button">;
+  href?: string;
+} & ComponentPropsWithRef<"button"> &
+  ComponentPropsWithRef<"a">;
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   // eslint-disable-next-line mobx/missing-observer
@@ -34,53 +37,56 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       rightIcon: RightIcon,
       classNames,
       block,
+      href,
       ...rest
     },
     ref,
   ) {
     const disabled = isLoading || buttonDisabled;
 
-    return (
-      <button
-        ref={ref}
-        type="button"
-        disabled={disabled}
-        className={cn(
-          "inline-flex items-center rounded font-medium",
-          "focus-visible:ring-primary-500 focus:outline-none focus-visible:ring",
-          "shadow-sm",
-          "transition-colors duration-75",
-          "disabled:shadow-inner",
-          [block && "w-full justify-center"],
-          //#region  //*=========== Size ===========
-          [size === "base" && ["px-3 py-3.5", "text-sm "]],
-          //#endregion  //*======== Size ===========
-          //#region  //*=========== Variants ===========
-          [
-            variant === "primary" && [
-              "border border-blue-600 bg-blue-600 text-white shadow",
-              "hover:bg-blue-700 hover:text-white",
-              "active:bg-blue-800",
-              "disabled:border-blue-600 disabled:bg-blue-600 disabled:opacity-30",
-            ],
-            variant === "outline" && [
-              "border border-gray-600 bg-transparent text-zinc-400",
-              "hover:bg-gray-700 hover:text-white",
-              "active:bg-gray-800",
-              "disabled:border-gray-500 disabled:opacity-30",
-            ],
-            variant === "confirmed" && [
-              "cursor-default border border-emerald-500 bg-emerald-500 text-white",
-            ],
-          ],
-          //#endregion  //*======== Variants ===========
-          "disabled:cursor-not-allowed",
-          isLoading &&
-            "relative text-transparent transition-none hover:text-transparent disabled:cursor-wait",
-          className,
-        )}
-        {...rest}
-      >
+    const style = cn(
+      "inline-flex items-center rounded font-medium",
+      "focus-visible:ring-primary-500 focus:outline-none focus-visible:ring",
+      "shadow-sm",
+      "transition-colors duration-75",
+      "disabled:shadow-inner",
+      [block && "w-full justify-center"],
+      //#region  //*=========== Size ===========
+      [size === "base" && ["px-3 py-3.5", "text-sm "]],
+      //#endregion  //*======== Size ===========
+      //#region  //*=========== Variants ===========
+      [
+        variant === "primary" && [
+          "border border-blue-600 bg-blue-600 text-white shadow",
+          "hover:border-blue-700 hover:bg-blue-700",
+          "active:bg-blue-800 active:hover:border-blue-800",
+          "disabled:border-blue-600 disabled:bg-blue-600 disabled:opacity-30",
+        ],
+        variant === "secondary" && [
+          "border border-gray-700 bg-gray-700 text-white shadow",
+          "hover:border-gray-800 hover:bg-gray-800 ",
+          "active:bg-gray-950 active:hover:border-gray-950",
+          "disabled:border-gray-700 disabled:bg-gray-700 disabled:opacity-50",
+        ],
+        variant === "outline" && [
+          "border border-gray-600 bg-transparent text-zinc-400",
+          "hover:bg-gray-700 hover:text-white",
+          "active:border-gray-800 active:bg-gray-800",
+          "disabled:border-gray-500 disabled:opacity-30",
+        ],
+        variant === "confirmed" && [
+          "cursor-default border border-emerald-500 bg-emerald-500 text-white",
+        ],
+      ],
+      //#endregion  //*======== Variants ===========
+      "disabled:cursor-not-allowed",
+      isLoading &&
+        "relative text-transparent transition-none hover:text-transparent disabled:cursor-wait",
+      className,
+    );
+
+    const ChildrenContent = () => (
+      <>
         {isLoading && (
           <div
             className={cn(
@@ -136,6 +142,22 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
             />
           </div>
         )}
+      </>
+    );
+
+    return href ? (
+      <Link href={href} className={style} {...rest}>
+        <ChildrenContent />
+      </Link>
+    ) : (
+      <button
+        ref={ref}
+        type="button"
+        disabled={disabled}
+        {...rest}
+        className={style}
+      >
+        <ChildrenContent />
       </button>
     );
   },
