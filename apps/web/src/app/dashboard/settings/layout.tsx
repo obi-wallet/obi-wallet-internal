f0009254659fe3a9805d5cd58e36ad60d422fa14
José Andradez <xjgamx@gmail.com>
2024-02-09 17:45:22 -0300

    Handle titles

---
+++ apps/web/src/app/dashboard/settings/layout.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import Head from "next/head";
+export const metadata: Metadata = {
+  title: "Obi - Settings",
+  description: "Anything in two clicks",
+};
+
+const layout = function ({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+};
+export default layout;
--- apps/web/src/app/onboarding/layout.tsx
+++ apps/web/src/app/onboarding/layout.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { Footer } from "@/components";
-import { ReactNode } from "react";
+import { Metadata } from "next";
 
-export default function OnboardLayout({ children }: { children: ReactNode }) {
+export default function OnboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <section className="flex w-full flex-col items-center justify-center max-sm:px-10">
       <div className="mt-24 w-fit grow max-sm:w-full">{children}</div>
