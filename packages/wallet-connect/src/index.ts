95a760a8d09b3354f006d4e6a439cc6a4f4299ea
Jonas Keinholz <git@inyono.dev>
2024-02-14 11:36:37 +0100

    automatically appprove session proposals

--- packages/wallet-connect/src/index.ts
+++ packages/wallet-connect/src/index.ts
@@ -2,7 +2,6 @@ import { Core } from "@walletconnect/core";
 import { buildApprovedNamespaces, getSdkError } from "@walletconnect/utils";
 import { Web3Wallet } from "@walletconnect/web3wallet";
 
-import { WalletConnectPairingUserInteraction } from "./user-interaction";
 export * from "./user-interaction";
 
 export async function setupWalletConnect({
@@ -43,7 +42,10 @@ export async function setupWalletConnect({
   web3wallet.on("session_proposal", async (params) => {
     console.log("incoming session_proposal", params);
 
-    const response = await WalletConnectPairingUserInteraction.start(params);
+    // Automatically approve the session proposal for now
+    const response = { approved: true };
+    // const response = await WalletConnectPairingUserInteraction.start(params);
+
     if (response.approved) {
       const accounts = await getAccounts();
       const chains = accounts.map((account) =>
@@ -74,38 +76,6 @@ export async function setupWalletConnect({
         reason: getSdkError("USER_REJECTED"),
       });
     }
-    const _examplePayload = {
-      id: 1707743595483114,
-      params: {
-        id: 1707743595483114,
-        pairingTopic:
-          "5f3a6ea5ba2337c3d4fd443f4ebb27032fa798c94271d77134d9f8047846adb1",
-        expiry: 1707743911,
-        requiredNamespaces: {
-          cosmos: {
-            methods: ["cosmos_signDirect", "cosmos_signAmino"],
-            chains: ["cosmos:cosmoshub-4"],
-            events: [],
-          },
-        },
-        optionalNamespaces: {},
-        relays: [
-          {
-            protocol: "irn",
-          },
-        ],
-        proposer: {
-          publicKey:
-            "5d31c5a6e03fd76e63209e1b684e94b5e49d550aa975b9d0ff752df8a1901113",
-          metadata: {
-            description: "React App for WalletConnect",
-            url: "https://react-dapp-v2-cosmos-provider.vercel.app",
-            icons: ["https://avatars.githubusercontent.com/u/37784886"],
-            name: "React App",
-          },
-        },
-      },
-    };
   });
 
   return web3wallet;
