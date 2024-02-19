e28b2b42d7fcee410e22bda21d3e9610dd37b95e
Jonas Keinholz <git@inyono.dev>
2024-02-19 11:47:06 +0100

    configurable fast travel asset via route param

--- apps/web/src/app/onboarding/fast-travel/[[...asset]]/page.tsx
+++ apps/web/src/app/onboarding/fast-travel/[[...asset]]/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { Stepper, TravelModal } from "@/components";
+import { observer } from "mobx-react-lite";
 import { useRouter } from "next/navigation";
 
-export default function Congratulations() {
+export default observer<{ params: { asset?: string[] } }>(function FastTravel({
+  params,
+}) {
   const router = useRouter();
 
   return (
     <section className="flex flex-col items-center space-y-7">
       <Stepper currentStep={2} totalSteps={2} />
       <TravelModal
-        targetAsset="sei"
+        targetAsset={params.asset?.[0] ?? "sei"}
         modal={false}
         onDismiss={() => {
           router.push("/dashboard");
@@ -18,4 +21,4 @@ export default function Congratulations() {
       />
     </section>
   );
-}
+});
