91275da538e7e0ab90a5ba7e2ec8ebc395e20146
Jonas Keinholz <git@inyono.dev>
2024-02-20 13:15:03 +0100

    use token registry in SendScreen

--- apps/web/src/app/dashboard/transaction/send/[[...asset]]/page.tsx
+++ apps/web/src/app/dashboard/transaction/send/[[...asset]]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { toAssets } from "@/app/dashboard/fast-travel/assets";
 import {
   BalanceInput,
   BalanceInputValue,
@@ -16,7 +15,6 @@ import {
 import { useCurrentWallet } from "@/hooks/use-current-wallet";
 import { usePublicKey } from "@/hooks/use-public-key";
 import { TargetChain } from "@/target-chain";
-import { isCosmosSdkChainId } from "@/target-chain/cosmos-sdk/chains";
 import { CosmosSdkMpcSigner } from "@/target-chain/cosmos-sdk/mpc-signer";
 import { Coin } from "@cosmjs/amino";
 import { MsgSendEncodeObject } from "@cosmjs/stargate";
@@ -46,18 +44,19 @@ export default observer<{ params: { asset?: string[] } }>(function Send({
       invariant(wallet, "Wallet not found");
       invariant(coin.asset, "No asset selected");
 
-      const chainId = coin.asset.asset.chainId;
-      invariant(
-        isCosmosSdkChainId(chainId),
-        "ChainId is not a Cosmos SDK chain",
-      );
+      const asset = coin.asset;
+
+      const chainId = coin.asset.targetChainId;
+      const denomUnit = asset.asset.denom_units.find((value) => {
+        return value.denom === asset.asset.display;
+      });
 
       const tokens: Coin[] = [
         {
           amount: new BigNumber(coin.amount)
-            .multipliedBy(10 ** coin.asset.asset.decimals)
+            .multipliedBy(10 ** (denomUnit?.exponent ?? 0))
             .toString(),
-          denom: coin.asset.asset.denom,
+          denom: asset.asset.base,
         },
       ];
 
@@ -125,28 +124,22 @@ export default observer<{ params: { asset?: string[] } }>(function Send({
 
   const balanceOptions = withChainId
     .map((b) => {
-      // find the asset in ToAssets using the denom, toAssets is an object and the denom is not the key
-      const asset =
-        toAssets[
-          Object.keys(toAssets).find(
-            (key) => toAssets[key]?.denom === b.denom,
-          ) ?? ""
-        ];
-      if (!asset) {
-        return null;
-      }
-      invariant(asset, "Asset not found");
-      const amount = Number(b.amount);
-      const decimals = asset?.decimals ?? 0;
-      // get amount using the asset's decimals
-      const decimalAmount = amount / Math.pow(10, decimals);
+      const assetData = TargetChain.chainId(b.chainId).getAsset(b.denom);
+      if (!assetData) return null;
+
+      const amount = new BigNumber(b.amount);
+      const denomUnit = assetData.denom_units.find((value) => {
+        return value.denom === assetData.display;
+      });
+      const decimalAmount = amount.dividedBy(10 ** (denomUnit?.exponent ?? 0));
 
       return {
-        image: asset?.image,
+        image: assetData.images?.[0]?.svg ?? assetData.images?.[0]?.png,
+        targetChainId: b.chainId,
         network: TargetChain.chainId(b.chainId).label,
-        assetUnit: asset?.label,
+        assetUnit: assetData?.symbol,
         balance: decimalAmount,
-        asset: asset,
+        asset: assetData,
       };
     })
     .filter((option): option is IBalanceOption => !!option);
