7435a4a9a7cf5185242cbaf4b4c60bfa665f244c
Jonas Keinholz <git@inyono.dev>
2024-02-26 14:22:03 +0100

    add automated test that checks backup shares can be decrypted successfully

--- apps/web/src/wallet-health/checks.stories.tsx
+++ apps/web/src/wallet-health/checks.stories.tsx
@@ -1,3 +1,6 @@
+import { HomeChain } from "@/home-chain";
+import { useCurrentWallet } from "@/hooks/use-current-wallet";
+import { MultisigKeyDecryption, Secp256k1Decryption } from "@/lib/encryption";
 import {
   AutomatedTest,
   automatedTestPlay,
@@ -7,7 +10,10 @@ import {
   usePublicKeyKnownCheck,
   useWalletBackupCheck,
 } from "@/wallet-health/checks";
+import { useQuery } from "@obi-wallet/headless-ui";
+import { KeyType } from "@obi-wallet/sdk";
 import { Meta, StoryObj } from "@storybook/react";
+import invariant from "tiny-invariant";
 
 const meta = {
   title: "Tests/wallet-health/checks",
@@ -46,4 +52,57 @@ export const UseWalletBackupCheck: Story = {
     );
   },
   play: automatedTestPlay,
+};
+
+export const EncryptedBackups: Story = {
+  decorators: [providerWithWalletDecorator],
+  render: function EncryptedBackupsTest() {
+    const wallet = useCurrentWallet({});
+    const query = useQuery({
+      queryKey: ["encrypted-backups", wallet?.userEntryAddress],
+      queryFn: async () => {
+        invariant(wallet, "Expected wallet to be set.");
+        const passkey = wallet.owner.getUsableKeyOfType(KeyType.Passkey);
+        invariant(passkey, "Expected passkey to be set");
+        const [data] = await HomeChain.chainId(
+          wallet.homeChainId,
+        ).lookupWalletBackup(passkey.publicKey);
+
+        invariant(data, "Expected data to be set");
+        invariant(passkey, "No usable passkey found");
+
+        const passkeyDecryption = new Secp256k1Decryption(
+          passkey.payload.privateKey,
+        );
+        const multisigKeyDecryption = new MultisigKeyDecryption([
+          passkey.payload.privateKey,
+        ]);
+
+        const [
+          actualEasyShare,
+          actualBackupShare,
+          backedUpEasyShare,
+          backedUpBackupShare,
+        ] = await Promise.all([
+          passkeyDecryption.decrypt(wallet.encryptedEasyShare),
+          multisigKeyDecryption.decrypt(wallet.encryptedBackupShare),
+          multisigKeyDecryption.decrypt(data.encryptedEasyShare ?? ""),
+          multisigKeyDecryption.decrypt(data.encryptedBackupShare),
+        ]);
+
+        return (
+          actualEasyShare === backedUpEasyShare &&
+          actualBackupShare === backedUpBackupShare
+        );
+      },
+      enabled: !!wallet,
+    });
+
+    return (
+      <AutomatedTest
+        done={query.isSuccess || query.isError}
+        success={query.isSuccess && query.data}
+      />
+    );
+  },
 };
