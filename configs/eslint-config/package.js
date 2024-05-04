035fdee63dd3626c323921b332413f7bafe3ed51
Jonas Keinholz <git@inyono.dev>
2024-05-04 16:15:27 +0200

    Enable @typescript-eslint/consistent-generic-constructors

--- configs/eslint-config/package.js
+++ configs/eslint-config/package.js
@@ -15,6 +15,7 @@ module.exports = {
   plugins: ["mobx", "import", "react", "react-hooks", "unicorn"],
   rules: {
     "@typescript-eslint/consistent-indexed-object-style": "error",
+    "@typescript-eslint/consistent-generic-constructors": "error",
     "@typescript-eslint/consistent-type-assertions": [
       "error",
       {
