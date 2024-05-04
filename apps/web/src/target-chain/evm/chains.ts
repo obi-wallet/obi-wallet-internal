7d900408c372450a43e74fcbab3679c152d96150
Jonas Keinholz <git@inyono.dev>
2024-05-04 21:08:42 +0200

    basic EvmTargetChain implementation

--- apps/web/src/target-chain/evm/chains.ts
+++ apps/web/src/target-chain/evm/chains.ts
@@ -1,3 +1,5 @@
+import { arbitrum, Chain } from "viem/chains";
+
 export enum EvmChainId {
   Arbitrum = "arbitrum",
 }
@@ -7,3 +9,18 @@ export function isEvmChainId(chainId: string): chainId is EvmChainId {
 }
 
 export const allEvmChainIds = [EvmChainId.Arbitrum];
+
+export interface EvmChainData {
+  id: EvmChainId;
+  chain: Chain;
+  image: string;
+  disabled?: boolean;
+}
+
+export const EvmChains: Record<EvmChainId, EvmChainData> = {
+  [EvmChainId.Arbitrum]: {
+    id: EvmChainId.Arbitrum,
+    image: "/assets/images/logo-arbitrum.png",
+    chain: arbitrum,
+  },
+};
