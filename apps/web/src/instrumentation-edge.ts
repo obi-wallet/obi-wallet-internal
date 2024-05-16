5a6efbbcce123f0e4422e675217a431291b3d6ec
Jonas Keinholz <git@inyono.dev>
2024-05-16 05:51:35 +0200

    reduce Sentry sample rates

--- apps/web/src/instrumentation-edge.ts
+++ apps/web/src/instrumentation-edge.ts
@@ -11,7 +11,7 @@ Sentry.init({
   release: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
