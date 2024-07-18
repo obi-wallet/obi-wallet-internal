f8afa72cb55fff276260eb6bcd1b5a8a8b1d5256
napster125 <napster0125@gmail.com>
2024-07-18 08:35:57 -0400

    initial commit for basic ui

--- apps/web/src/app/dashboard/view-points/page.tsx
+++ apps/web/src/app/dashboard/view-points/page.tsx
@@ -1,38 +1,40 @@
 "use client";
 
-import { Box, Input, Button } from "@/components";
+import { Box, Button } from "@/components";
 import { observer } from "mobx-react-lite";
+import Image from "next/image";
 
 export default observer(function ViewPoints() {
   return (
     <div className="w-full ">
-      <Box className="w-full lg:w-1/2 bg-panel-gradient relative flex flex-col gap-4 rounded-tl-[10px] rounded-tr-[10px]">
-        <div className="flex-1 text-center text-white">
-          Import New Asset
+      <Box className="w-full lg:w-1/2 bg-panel-gradient relative flex flex-col gap-4 rounded-tl-[10px] rounded-tr-[10px] font-press-start-2p">
+        <div className="text-white flex flex-col justify-between gap-4 text-[12px] font-regular">
+          <div className="flex justify-between items-center w-full">
+            <h4 className="text-[18px] font-regular">Score: 000069</h4>
+          </div>
+          <div className="flex justify-between items-center w-full">
+            <p className="drop-shadow-[2px_2px_2px_rgb(0,0,0)]">Create Account</p>
+            <p className="flex items-center gap-1"><Image src="/Clip path group.png" height={14} width={16} alt="star"/>69</p>
+          </div>
+          <div className="flex justify-between items-center w-full opacity-50">
+            <p className="drop-shadow-[2px_2px_2px_rgb(0,0,0)]">Use Tunnel</p>
+            <p className="flex items-center gap-1"><Image src="/Clip path group.png" height={14} width={16} alt="star"/>1/$</p>
+          </div>
+          <div className="flex justify-between items-center w-full opacity-50">
+            <p className="drop-shadow-[2px_2px_2px_rgb(0,0,0)]">Extra Life</p>
+            <p className="flex items-center gap-1"><Image src="/Clip path group.png" height={14} width={16} alt="star"/>99</p>
+          </div>
+          <div className="flex justify-between items-center w-full opacity-50">
+            <p className="drop-shadow-[2px_2px_2px_rgb(0,0,0)]">Do Something</p>
+            <p className="flex items-center gap-1"><Image src="/Clip path group.png" height={14} width={16} alt="star"/>69</p>
+          </div>
         </div>
-        <div>
-          <label className="text-sm text-white">Token contract address</label>
-          <Input className="mt-2 px-3 py-3 text-sm" />
-        </div>
-        <div>
-          <label className="text-sm text-white">Token Symbol</label>
-          <Input className="mt-2 px-3 py-3 text-sm" />
-        </div>
-        <div>
-          <label className="text-sm text-white">Token Decimal</label>
-          <Input className="mt-2 px-3 py-3 text-sm" />
-        </div>
-        <div className="mb-4 mt-0.5 flex gap-8 text-white">
-          <Button
-            href="/dashboard"
-            className="flex-1 justify-center rounded-lg border-blue-500 bg-transparent p-2 text-center"
-          >
-            Cancel
-          </Button>
-          <Button href="" className="flex-1 justify-center rounded-lg p-2">
-            Import
-          </Button>
-        </div>
+        <Button
+          className="p-0 text-[14px] bg-transparent border-0 hover:bg-transparent absolute top-2 right-4 "
+          href="/dashboard/settings"
+        >
+          x
+        </Button>
       </Box>
     </div>
   );
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      dropShadow: {
+        "dropScore": '0 35px 35px rgba(0, 0, 0, 0.25)'
+      },
       colors: {
         "background-main": "var(--background-main)",
         "background-primary": "var(--background-primary)",
