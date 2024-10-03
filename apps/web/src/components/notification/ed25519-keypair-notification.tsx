b5cdcba6cd23a91e7d5c8af717f09469e730e9d6
Jonas Keinholz <git@inyono.dev>
2024-10-03 15:51:03 +0200

    improve message for Ed25519KeyPairNotification

--- apps/web/src/components/notification/ed25519-keypair-notification.tsx
+++ apps/web/src/components/notification/ed25519-keypair-notification.tsx
@@ -47,7 +47,7 @@ export const Ed25519KeyPairNotification = observer(
 
     return (
       <Notification
-        description="Caution: Your account does not have an ed25519 key pair yet."
+        description="Your wallet needs a quick upgrade to work with Solana. Click here to upgrade now."
         type="warning"
         onClick={async () => {
           invariant(currentWallet, "Expected current wallet to be defined");
