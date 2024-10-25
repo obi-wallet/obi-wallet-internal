63b8f54e31242ff3d12fb6423b7a7e0e2d3ce813
Jonas Keinholz <git@inyono.dev>
2024-10-25 12:14:25 +0200

    Delete unused abstract SDK files
    Removed deprecated SDK files for bank, contracts, gatekeeper, and staking modules.

--- packages/sdk/src/sdk/sdk/secret-js.ts
+++ packages/sdk/src/sdk/sdk/secret-js.ts
@@ -1,26 +1,14 @@
 import { AbstractSdk } from "./abstract";
 import { SecretJsClient } from "../../clients";
 import { SecretJsHomeChainId } from "../../home-chains";
-import { SecretJsBankSdk } from "../bank";
-import { SecretJsContractsSdk } from "../contracts";
-import { NotImplementedGatekeeperSdk } from "../gatekeeper";
-import { NotImplementedStakingSdk } from "../staking";
 import { SecretJsTransactionsSdk } from "../transactions";
 
 export class SecretJsSdk extends AbstractSdk {
-  public bank: SecretJsBankSdk;
-  public contracts: SecretJsContractsSdk;
-  public gatekeeper: NotImplementedGatekeeperSdk;
-  public staking: NotImplementedStakingSdk;
   public transactions: SecretJsTransactionsSdk;
 
   protected constructor(protected override chainId: SecretJsHomeChainId) {
     super(chainId);
     const client = new SecretJsClient(chainId);
-    this.bank = new SecretJsBankSdk({ chainId });
-    this.contracts = new SecretJsContractsSdk({ chainId, client });
-    this.gatekeeper = new NotImplementedGatekeeperSdk(chainId);
-    this.staking = new NotImplementedStakingSdk(chainId);
     this.transactions = new SecretJsTransactionsSdk({ chainId, client });
   }
 
--- packages/sdk/src/sdk/staking/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./abstract";
-// export * from "./cosm-js";
-// export * from "./feather-js";
-export * from "./not-implemented";
