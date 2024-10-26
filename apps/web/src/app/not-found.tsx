67cd43dd92d1e8b453f25275ec81b6e4c23b62df
Jonas Keinholz <git@inyono.dev>
2024-10-26 03:03:04 +0200

    Create reusable animation components for alerts
    Refactored alert animations into reusable Lottie components for success, error, and warning alerts. Updated existing alert usages to utilize the new components and consolidated alert types into a single enum.

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,11 +1,11 @@
 "use client";
-import Danger from "@/alert/danger.json";
-import Lottie from "lottie-react";
+
+import { DangerAnimation } from "@/animations/danger";
 
 export default function NotFound() {
   return (
     <div className="m-auto text-center text-white">
-      <Lottie animationData={Danger} loop={false} height={300} width={300} />
+      <DangerAnimation height={300} width={300} />
       <h1 className="mb-4 text-4xl">404 | Not Found</h1>
       <p>Could not find requested resource</p>
     </div>
