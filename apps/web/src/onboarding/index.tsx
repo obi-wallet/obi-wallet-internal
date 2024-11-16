54322fb0eddeb3696a1224a4bc6978d425d79c72
rustopian <pete@obi.money>
2024-11-16 18:28:09 +0000

    remove stepper

--- apps/web/src/onboarding/index.tsx
+++ apps/web/src/onboarding/index.tsx
@@ -45,7 +45,7 @@ export const Onboarding = observer(function Onboarding({
 
   return (
     <section className="flex flex-col items-center space-y-7">
-      <Stepper currentStep={step + 1} totalSteps={steps.length} />
+      {/* <Stepper currentStep={step + 1} totalSteps={steps.length} /> */}
       <Step draft={draft} step={currentStep} back={back} next={next} />
     </section>
   );
