116b8ccd9164bfe59a6b2abb3e3f8e5cc94941a6
rustopian <pete@obi.money>
2024-11-18 19:46:39 +0000

    starting send/receive style

--- apps/web/src/app/dashboard/transaction/layout.tsx
+++ apps/web/src/app/dashboard/transaction/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, TabUi } from "@/components";
+import { Box } from "@/components";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
@@ -15,22 +15,51 @@ export default function TransactionLayout({
   return (
     <div className="w-full">
       <Box className="w-full xl:w-1/2">
-        <TabUi.Links>
-          <TabUi.Link
+        <div className="flex gap-2.5 mb-4">
+          <TabLink
             href={`/dashboard/transaction/send/${rest.join("/")}`}
             active={activeTab === "send"}
           >
-            Send Tokens
-          </TabUi.Link>
-          <TabUi.Link
+            Send
+          </TabLink>
+          <TabLink
             href={`/dashboard/transaction/receive/${rest.join("/")}`}
             active={activeTab === "receive"}
           >
-            Receive Tokens
-          </TabUi.Link>
-        </TabUi.Links>
+            Receive
+          </TabLink>
+        </div>
         {children}
       </Box>
     </div>
+  );
+}
+
+interface TabLinkProps {
+  href: string;
+  active: boolean;
+  children: React.ReactNode;
+}
+
+function TabLink({ href, active, children }: TabLinkProps) {
+  return (
+    <a
+      href={href}
+      className={
+        active
+          ? "w-full h-11 p-2.5 bg-[#32c9af] rounded-[5px] flex items-center justify-center"
+          : "w-full h-11 p-2.5 bg-[#353535] rounded-[5px] flex items-center justify-center"
+      }
+    >
+      <span
+        className={
+          active
+            ? "text-[#070707] text-lg font-normal font-['Roboto Mono']"
+            : "text-white text-lg font-normal font-['Roboto Mono']"
+        }
+      >
+        {children}
+      </span>
+    </a>
   );
 }
