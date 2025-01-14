0b1948b6128795877ed3fa81f2e1c64cda0c627d
Jonas Keinholz <git@inyono.dev>
2025-01-14 12:37:23 +0100

    Show target asset info during Status

--- apps/web/src/tunnel/state-handler/status.tsx
+++ apps/web/src/tunnel/state-handler/status.tsx
@@ -2,6 +2,7 @@
 
 import { Text } from "@/components";
 import { EffectStateDispatch } from "@/effect/effect-state";
+import { useAssets } from "@/hooks/assets";
 import { AsyncButton } from "@/ui/button";
 import { Input } from "@/ui/input";
 import { observer } from "mobx-react-lite";
@@ -13,14 +14,19 @@ export interface StatusProps {
   dispatch: EffectStateDispatch<typeof TunnelState>;
 }
 
-export const Status = observer<StatusProps>(function Deposit() {
+export const Status = observer<StatusProps>(function Deposit({ state }) {
+  const assets = useAssets([state.from.asset, state.to.asset]);
+  const fromAsset = assets[state.from.asset];
+  const toAsset = assets[state.to.asset];
+
   return (
     <div className="bg-background-main flex min-h-screen flex-col justify-center p-8 text-white">
       <Text size="xl" className="flex items-center gap-2">
-        {/* TODO: get asset name & amount */}
-        You are expected to receive XXX $XYZ
+        You are expected to receive {state.to.prettyAmount} $
+        {toAsset?.assetInfo?.symbol ?? toAsset?.denom}
       </Text>
       <Text className="mt-4">
+        {/* TODO: handle wallet type */}
         {/* TODO: label instead */}
         <span className="align-middle leading-normal">Receiving address:</span>
       </Text>
@@ -32,19 +38,21 @@ export const Status = observer<StatusProps>(function Deposit() {
           console.log("continue");
         }}
       >
-        XXXX
+        {state.to.address}
       </AsyncButton>
       <Text className="mt-4">
         {/* TODO: label instead */}
         <span className="align-middle leading-normal">
-          Please send YYY $YXZ to the address below to complete the transaction:
+          Please send {state.from.prettyAmount}{" "}
+          {fromAsset?.assetInfo?.symbol ?? fromAsset?.denom} to the address
+          below to complete the transaction:
         </span>
       </Text>
       <Input
         labelClassname="bg-background-secondary"
         className="mt-2 h-[48px] w-full rounded-[5px] border border-[#32c9af]"
         // TODO: handle value, copy-paste
-        value="0x1234567890"
+        value={state.from.address}
         // value={field.value}
         // onChange={(recipient) => {
         //   field.onChange(recipient);
