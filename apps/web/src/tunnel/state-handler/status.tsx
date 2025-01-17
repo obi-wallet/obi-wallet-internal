d9f9fe3d7b7d17aa7e1630a9e015076fa349ac9e
Jonas Keinholz <git@inyono.dev>
2025-01-17 09:24:21 +0100

    Allow copying the deposit address

--- apps/web/src/tunnel/state-handler/status.tsx
+++ apps/web/src/tunnel/state-handler/status.tsx
@@ -5,7 +5,10 @@ import { EffectStateDispatch } from "@/effect/effect-state";
 import { useAssets } from "@/hooks/assets";
 import { AsyncButton } from "@/ui/button";
 import { Input } from "@/ui/input";
+import copy from "copy-to-clipboard";
 import { observer } from "mobx-react-lite";
+import { useState } from "react";
+import { FaCheck } from "react-icons/fa6";
 
 import { StatusState, TunnelState } from "../state";
 import { GenericWallet, ObiWallet, PhantomWallet } from "../wallets";
@@ -22,6 +25,7 @@ export const Status = observer<StatusProps>(function Deposit({
   const assets = useAssets([state.from.asset, state.to.asset]);
   const fromAsset = assets[state.from.asset];
   const toAsset = assets[state.to.asset];
+  const [isCopied, setIsCopied] = useState(false);
 
   const Wallet =
     state.walletType === "phantom"
@@ -57,11 +61,32 @@ export const Status = observer<StatusProps>(function Deposit({
         <Input
           labelClassname="bg-background-secondary"
           className="mt-2 h-[48px] w-full rounded-[5px] border border-[#32c9af]"
-          // TODO: handle value, copy-paste
           value={state.from.address}
           readOnly
+          rightComponent={
+            <div className="flex w-full justify-end">
+              <button
+                onClick={() => {
+                  copy(state.from.address);
+                  setIsCopied(true);
+
+                  setTimeout(() => {
+                    setIsCopied(false);
+                  }, 2000);
+                }}
+              >
+                <img src="/assets/icons/copy.svg" alt="copy" />
+              </button>
+            </div>
+          }
         />
       </label>
+      {isCopied && (
+        <div className="mt-2 flex items-center gap-2 text-green-500">
+          <FaCheck />
+          <p className="text-white">Copied</p>
+        </div>
+      )}
       <AsyncButton
         className="mt-2 w-full"
         variant="outline"
